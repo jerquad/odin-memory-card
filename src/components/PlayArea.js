769b96a45df7c04c62cf44795899ad217b9a2095
Jimmy Quadros <jimmyquadros@hotmail.com>
2023-01-22 10:20:45 -0500

    implement level status indicator

--- src/components/PlayArea.js
+++ src/components/PlayArea.js
@@ -6,6 +6,12 @@ function PlayArea(props) {
     const [images, setImages] = useState(props.images);
     const [marked, setMarked] = useState(() => new Array(props.level + 2).fill(false));
 
+    const [roundStatus, setRoundStatus] = useState(() => makeStatus());
+
+    useEffect(() => {
+        setRoundStatus(makeStatus());
+    }, [images]);
+
     function randomizeImages() {
         const [lvl, mrk] = props.random(images, marked);
         setImages(lvl);
@@ -47,17 +53,31 @@ function PlayArea(props) {
         setMarked(mark);
     }
 
+    function makeStatus() {
+        const status = [];
+        let i = 1;
+        while (i <= images.length) {
+            if (i < roundScore) status.push((<div key={i.toString()} className='stat-box stat-check'></div>))
+            else status.push((<div key={i.toString()} className='stat-box'></div>))
+            i++
+        }
+        return status;
+    }
+
     return (
-        <div id='play-images'>
-            {images.map((img, i) => 
-                <img
-                    key={i.toString()}
-                    src={img}
-                    alt={`dummy-${i}`}
-                    onClick={(e) => {
-                        handleClick(i)
-                    }}
-                />)}
+        <div id='pa-container'>
+            <div id='round-status'>{roundStatus}</div>
+            <div id='play-images'>
+                {images.map((img, i) =>
+                    <img
+                        key={i.toString()}
+                        src={img}
+                        alt={`dummy-${i}`}
+                        onClick={(e) => {
+                            handleClick(i)
+                        }}
+                    />)}
+            </div>
         </div>
     );
 }
